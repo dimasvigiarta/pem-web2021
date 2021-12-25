2ffcee0696d75910f8137c493a7c028f5bd0b9c6
dimasvigiarta <91957792+dimasvigiarta@users.noreply.github.com>
2021-12-25 18:38:40 +0700

    minggu12

---
+++ minggu12/praktikum9.js
@@ -0,0 +1,15 @@
+var person = {
+    firstName: "Agus",
+    lastName: "Santoro",
+    showName: function(){
+        alert(`Nama: ${this.firstName} ${this.lastName}`);
+    }
+};
+
+   var person2 = {
+    firstName: "Belajar",
+    lastName: "Kode",
+    showName: function(){
+        alert(`Nama: ${this.firstName} ${this.lastName}`);
+    }
+};
